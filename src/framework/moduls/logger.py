f49c501cb11ba50c0244f0948b893112e0b51c2c
Nyikes Dániel <nyikesda@gmail.com>
2016-10-27 00:09:15 +0200

    [BUGFIX] Remove logger previous file handlers

--- src/framework/moduls/logger.py
+++ src/framework/moduls/logger.py
@@ -2,9 +2,9 @@ from moduls.schemaSpecificAttr import *
 import logging
 
 class AKEPLogger:
-    def initialize(fileName):
+    def initialize(fileName, title = None):
         # create AKEP logger
-        logger = logging.getLogger(LOGGER_ID)
+        logger = logging.getLogger(LOGGER_ID if title is None else (LOGGER_ID + '-' + title))
         logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -16,10 +16,13 @@ class AKEPLogger:
         ch = logging.StreamHandler()
         ch.setLevel(logging.ERROR)
         ch.setFormatter(formatter)
-
+        
+        for handler in logger.handlers:
+            logger.removeHandler(handler)
         logger.addHandler(fh)
-        logger.addHandler(ch)
+        logger.addHandler(ch)        
         return logger
-    
+
+
     def getLogger(postTitle):
         return logging.getLogger(LOGGER_ID+'.'+postTitle)
